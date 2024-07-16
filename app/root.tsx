3365c6dcc3c4813ec54a6afa09febba3efa56039
eMKa18 <1918102+eMKa18@users.noreply.github.com>
2024-07-16 09:57:39 +0200

    Adding fading effect while page is loading details

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 
 import { json, LinksFunction, redirect } from "@remix-run/node";
@@ -30,6 +31,8 @@ export const loader = async () => {
 
 export default function App() {
   const {contacts} = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+
   return (
     <html lang="en">
       <head>
@@ -92,7 +95,9 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div 
+          className={navigation.state === "loading" ? "loading" : ""}
+          id="detail">
           <Outlet />
         </div>
 
