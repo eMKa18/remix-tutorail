f84914c0b992245d7f6fa24fa4adf2568e317236
eMKa18 <1918102+eMKa18@users.noreply.github.com>
2024-07-16 09:53:09 +0200

    Adding NavLink and styles for the active and pending skills

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 import {
   Form,
-  Link,
+  NavLink,
   Links,
   Meta,
   Outlet,
@@ -9,14 +9,14 @@ import {
   useLoaderData,
 } from "@remix-run/react";
 
-import { json, LinksFunction } from "@remix-run/node";
+import { json, LinksFunction, redirect } from "@remix-run/node";
 import appStylesHref from "./app.css?url";
 
 import { createEmptyContact, getContacts } from "./data";
 
 export const action = async () => {
   const contact = await createEmptyContact();
-  return json({contact});
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export const links: LinksFunction = () => [
@@ -62,7 +62,15 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink className={({isActive, isPending}) => 
+                      isActive 
+                        ? "active"
+                        : isPending 
+                        ? "pending"
+                        : ""
+                    }
+                    to={`contacts/${contact.id}`}
+                    >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -73,7 +81,7 @@ export default function App() {
                       {contact.favorite ? (
                         <span>★</span>
                       ) : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
@@ -82,14 +90,6 @@ export default function App() {
                 <i>No contacts</i>
               </p>
             )}
-            <ul>
-              <li>
-                <Link to={`/contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link to={`/contacts/2`}>Your Friend</Link>
-              </li>
-            </ul>
           </nav>
         </div>
         <div id="detail">
